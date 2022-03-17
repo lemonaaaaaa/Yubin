51c66d56bf47efe83f5d062ad4d682ac7f24dfa7
admin <admin@YD-08>
2022-03-18 08:59:32 +0900

    0317 집

--- HelloJava/src/com/dev/BankApp.java
+++ HelloJava/src/com/dev/BankApp.java
@@ -2,11 +2,18 @@ package com.dev;
 
 import java.util.Scanner;
 
-//BankApp(main method), Account(계좌번호, 예금주, 잔액)
-public class BankApp {
+// BankApp(main method), Account(계좌번호,예금주,잔액) 
+public class BankApp { // BankApp 클래스를 실행, 메인메소드를 우선순위로 실행. [line 17]
 	static Account[] banks = new Account[100];
 	static Scanner scn = new Scanner(System.in);
-	
+
+	// 메뉴: 1.계좌생성(번호,예금주,예금액)
+	// 2.예금(번호,예금액) -> 최고예금액 100,000원.
+	// 3.출금(번호,출금액) -> 잔액보다 큰 금액 출금 못하도록.
+	// 4.잔액조회(번호)
+	// 5.종료.
+	// 오늘이 자바 4일째인데 ... 벅찬 프로그램입니다...
+
 	public static void main(String[] args) {
 
 		while (true) {
@@ -14,81 +21,111 @@ public class BankApp {
 			int menu = scn.nextInt();
 
 			if (menu == 1) {
-				createAccount();
+				createAccout();
 			} else if (menu == 2) {
 				deposit();
 			} else if (menu == 3) {
+
 				withdraw();
 			} else if (menu == 4) {
-				System.out.println("프로그램 종료");
+				findAccountMoney();
+			} else if (menu == 5) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else if (menu == 9) {
+				showList();
 			}
+
 		}
-	}
+		System.out.println("=== End of Prog ===");
+	} // end of main()
 
-	// 메뉴: 1.계좌생성(번호, 예금주, 예금액)
-	// 2. 예금(번호입력, 예금액 입력)==>최고 10만원까지만 가능.
-	// 3. 출금(번호입력, 출금액) ==>잔고 이상의 금액은 출금 못하도록.
-	// 4. 잔액조회(번호)
-	// 5. 종료.
-
-	// 메뉴출력기능
+	// 메뉴출력 기능.
 	public static void printMenu() {
-		String menu = "1.계좌생성(번호, 예금주, 예금액)\r\n" //
-	+ "2. 예금(번호입력, 예금액 입력)==>최고 10만원까지만 가능.\r\n"//
-	+ "3. 출금(번호입력, 출금액) ==>잔고 이상의 금액은 출금 할수없음.\r\n"// 
-	+ "4. 잔액조회(번호)" + "5. 종료.\r\n" + "선택>";
+		String menu = "=========================================================\r\n" 
+				+ "1.계좌생성(번호,예금주,예금액)\r\n" 
+				+ "2.예금(번호,예금액) -> 최고예금액 100,000원.\r\n"
+				+ "3.출금(번호,출금액) -> 잔액보다 큰 금액 출금 못하도록.\r\n" 
+				+ "4.잔액조회(번호)\r\n"
+				+ "5.종료.\r\n"
+				+ "=========================================================\r\n" + "+선택>	";
 
 		System.out.println(menu);
 	}
 
-	// 계좌 생성
-	public static void createAccount() {
-		System.out.println("계좌생성");
-		System.out.print("계좌번호 입력>>");
-		String accNo = scn.next();
-		System.out.println("예금주 이름 입력");
+	// 계좌생성 메소드.
+	public static void createAccout() {
+		System.out.println("계좌생성가능.");
+		System.out.print("계좌번호입력>> ");
+		String accNo = scn.next(); // 사용자의 코드를 문자열 형태로 반환해준다.
+		System.out.print("예금주입력 >>");
 		String accName = scn.next();
-		System.out.println("예금액 입력");
-		String accMoney = scn.next();
-		Account account = new Account(accNo, accName, accMoney){// new account(
-		System.out.println("계좌가 정상적으로 생성되었습니다");
-		
+		System.out.print("예금액입력 >>");
+		int accMoney = scn.nextInt();
+		Account accnt = new Account(accNo, accName, accMoney); // 초기화하는데, 이러한 것들을 가지고 초기화 하겠습니다.
+
+		for (int i = 0; i < banks.length; i++) {
+			if (banks[i] == null) {
+				banks[i] = accnt;
+				break;
+
+			}
+		}
+
+		System.out.println("계좌가 정상적으로 생성되었습니다.");
 	}
 
-	// 예금처리
+	// 예금 메소드.
 	public static void deposit() {
-		System.out.println("예금");
-		System.out.println("예금주 이름 입력");
-		String accName = scn.next();
-		System.out.println("예금액 입력");
-		int accMoney = scn.nextInt();
-		
-		int checkCnt = 0;
-		for(int i= 0; i < banks.length; i++) {
-			if(banks[i].getAccNo().equals(accMoney)) {
-				int currAmt = banks[i].getMoney();
+		System.out.println("예금기능.");
+		System.out.println("계좌번호>>");
+		String ano = scn.next();
+		System.out.println("예금액 입력>>");
+		int amt = scn.nextInt();
+		int checkCnt = 0; // 조회가 됐는지 체크 여부 변수.
+		for (int i = 0; i < banks.length; i++) {
+			if (banks[i] != null && banks[i].getAccNo().equals(ano)) { // 계좌번호 있을 경우..
+				// 계좌번호 조회됐을 때..
 				checkCnt = 1;
-				int currAmt = banks[i]
+				int currAmt = banks[i].getMoney();
 				
+				// 예금액이 10만원을 초과하지  못하도록..
+				if (currAmt + amt > 100000) {
+					checkCnt = 2;
+					break;
+				}
+				banks[i].setMoney(currAmt + amt); // 잔액 + 입금액
+				break;
 			}
+		}
+		if (checkCnt == 1) {
+
+			System.out.println("정상적으로 처리되었습니다.");
+		} else if (checkCnt == 2) {
+			System.out.println("한도금액을 초과했습니다.");
+
+		} else {
+			System.out.println("계좌번호가 없습니다.");
+
 		}
 	}
 
-	// 출금처리
+	// 출금 메소드.
 	public static void withdraw() {
-		System.out.println("출금");
-		System.out.println("예금주 이름 입력");
-		String accName = scn.next();
-		System.out.println("출금액 입력");
-		int accMoney = scn.nextInt();
+		System.out.println("출금기능.");
 	}
 
-	// 잔액조회
+	// 잔액조회 메소드.
 	public static void findAccountMoney() {
-		System.out.println("조회기능");
-		System.out.println("계좌번호 입력");
-		System accNo = scn.next();
-		System.out.println("현재 잔액은");
+		System.out.println("조회기능.");
 	}
-//전체 리스트 출력
+
+	// 전체리스트 출력.
+	public static void showList() {
+		for (int i = 0; i < banks.length; i++) {
+			if (banks[i] != null) {
+				System.out.println(banks[i].toString());
+			}
+		}
+	}
 }
--- HelloJava/src/com/dev/GugudanEx.java
+++ HelloJava/src/com/dev/GugudanEx.java
@@ -1,79 +1,69 @@
 package com.dev;
 
 public class GugudanEx {
-	public static void getScore() { // void 앞에 static이 있으면 인스턴스를 안해도(초기값을 설정 안해도) 바로 가져옴
 
-	}
-	
 	public static void main(String[] args) {
-		int result = sum(sum(10,20), sum(5,7));
-		int[]newAry = { 10, 20, 30};
+		int result = sum(sum(10, 20), sum(5, 7));
+		int[] newAry = {10,20,30};
 		result = sum(newAry);
 		System.out.println("결과는 " + result);
+//		System.out.println("결과는 " + divide(10, 4));
 	}
+
 	public static int sum(int[] ary) {
-		//배열매개변수의 각 요소의 합을 구하는 메소드
-		int sum = 0;
-		for(int i = 0; i <ary.length; i++) {
+		// 배열매개변수의 각 요소의 합을 구하는 메소드.
+		int sum =0;
+		for(int i=0;i<ary.length;i++) {
 			sum += ary[i];
-					
-		}return sum;
-		
-		
+		}
+		return sum;
 	}
-	
-	
-	
-	//나누기 메소드(int, int)
-	public static double devide(int n1, int n2) {
-		if (n2== 0) {
+
+	// 나누기 메소드( int , int )
+	public static double divide(int n1, int n2) {
+		if (n2 == 0) {
 			return 0;
 		}
 		return n1 / (double) n2;
-		
+
 	}
-	
+
 	public static int sum(int n1, int n2) {
 		return n1 + n2;
-		
 	}
 
-	public static void printstar(int runCnt, String shape) {
-			for(int i = 0; i<runCnt; i++) {
-			System.out.println();
-	
-	}
-
-	public static void gogodan(int dan) {
-		System.out.println("[" + dan + "단]");
+	public static void gugudan(int dan) {
+		System.out.println("     [" + dan + "단] ");
 		for (int i = 1; i <= 9; i++) {
 			System.out.printf("%2d * %2d = %2d\n", dan, i, (i * dan));
 		}
 		System.out.println();
+	}
+
+	public static void printStar(int runCnt, String shape) {
+		for (int i = 0; i < runCnt; i++) {
+			System.out.print(shape);
+		}
 
 	}
 
-	public static void Gugudan() {
-		// static 정적.method실행하려면 new 클래스이름(); ex) Student s1 = new Student();
-		// Student.callStatic();
-		for (int n = 2; n <= 9; n++) {
-			System.out.print("[" + n + "단]\t");
-		}
+	public static void printGugudan() {
 
-		System.out.println();
-		for (int n = 2; n <= 9; n++) {
+		// 메소드 실행하려면 new 클래스이름(); ex) Student s1 = new Student();
+//		Student.callStatic();
+		for (int j = 1; j <= 9; j++) { // 중첩for문
+			int cnt = j;
+//			System.out.println("[" + cnt + "단] ");
 			for (int i = 2; i <= 9; i++) {
-				System.out.print(i + "*" + n + "=" + n * i + "\t");
+				System.out.printf("%2d * %2d = %2d ", i, cnt, (i * cnt));
 			}
 			System.out.println();
-
 		}
-	}// end of 구구단
+	} // end of printGugudan();
 
 //	public static void main(String[] args) {
-//		printstar(10, "ㅗㅗ");
-//		gogodan(3);
+//
+//		printStar();
+//	}
 
-	}// end of program
-
-}
+} // end of GugudanEx
