12b3f5aea672ddcefc964329d2f1c41fd73cd1ae
user <user@DESKTOP-2203084>
2022-03-21 07:01:48 +0900

    0321아침

--- P20220318/src/com/test/Exam05.java
+++ P20220318/src/com/test/Exam05.java
@@ -17,56 +17,80 @@ public class Exam05 {
 
 	public static void main(String[] args) {
 
-		public static void printMenu() {
-			String menu = "===============================================\r\n" //
-					+ "1.책 등록"
-					+ "2.책 조회  " + "==============================================\r\n" //
-					+ "+선택>	";
-
-			System.out.println(menu);
-			
 		while (true) {
 			printMenu();
 			int menu = scn.nextInt();
 
-//			if (menu == 1) {
-//				creatBookNa();
-//			} else if (menu == 2) {
-//				creatBookWr();
-//			} else if (menu == 3) {
-//				creatBookPr();
+			if (menu == 1) {
+				createBookNa();
+			} else if (menu == 2) {
+				showBook();
+			} else if (menu == 3) {
+				System.out.println("프로그램을 종료합니다");
 //			} else if (menu == 4) {
-//				creatBookMo();
+//				creatBookWr();
 //			} else if (menu == 5) {
-//				showBook();
+//				creeatBookPr();
 //			} else if (menu == 6) {
-//				System.out.println("프로그램을 종료합니다");
-//				break;
-			}
-			public static void creatBookNa();
+//				creatBookMo();
+//			}
+
+		}
+		System.out.println("end of program");
+		}
+	}// end of main
+
+	// 메뉴
+	public static void printMenu() {
+		String menu = "===============================================\r\n" //
+				+ "1.책 등록" //
+				+ "2.책 조회" //
+				+ "3.종료" + "==============================================\r\n" //
+				+ "+선택>	";
+
+		System.out.println(menu);
+	}
+
+//책 정보 생성 메소드
+	public static void createBookNa() {
+		System.out.println("책 생성 기능입니다");
+
+		while (true) {
+
 			System.out.println("책이름을 입력해주세요");
 			String inputNa = scn.next();
-			
-			public static void creatBookWr();
-			System.out.println("저자를 입력해주세요");
-			String inputWr = scn.next();
-			
-			public static void creatBookPr();
-			System.out.println("출판사를 입력해주세요");
-			String inputPr = scn.next();
-			
-			public static void creatBookMo();
-			System.out.println("가격을 입력해 입력해주세요");
-			int inputMo = scn.nextInt();
-			
-			
+			if (createBookNa(inputNa) != null) {
+				System.out.println("이미 등록된 책입니다");
+				continue;
+			}
+			break;
 		}
-
+		public static void createBookWr() {
+		System.out.println("저자를 입력해주세요");
+		String inputWr = scn.next();
+		}
+		public static void createBookWr() {
+		System.out.println("출판사를 입력해주세요");
+		String inputPr = scn.next();
+		}
+		public static void createBookMo() {
+		System.out.println("가격을 입력해 입력해주세요");
+		int inputMo = scn.nextInt();
+		}
 		System.out.println("책 등록이 완료되었습니다");
 	}
 
-	// 메뉴
+	public static void showBook() {
+		System.out.println("책 제목을 입력해 주세요");
+		for (int i = 0; i < books.length; i++) {
+			if (books[i].getBookName() != null) {
+				System.out.printf(books[i].getBookName(), books[i].getBookWriter(), //
+						books[i].getBookPrint(), books[i].getBookPrice());
+				{
+				}
 
-		
-		
+			}
+
+		}
+	}
 }
