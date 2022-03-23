ea932e756c4e4797a38b9996921a3e100eef3fb8
admin <admin@YD-08>
2022-03-23 19:07:10 +0900

    울면서 고친 내 코드들 0322

---
+++ P20220318/src/com/test/Exam052.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import java.util.Scanner;
+
+//문제5) Book클래스를 선언(필드:책제목, 저자, 출판사, 금액) 
+//생성자(책제목, 저자, 출판사, 금액)를 선언 후 Scanner클래스를 사용하여 
+//사용자의 입력을 통하여 인스턴스를 생성하고 
+//입력한 내용을 책제목, 저자, 출판사, 금액 정보를 화면에 출력하는 기능 구현.
+public class Exam052 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("책 제목을 입력하세요 : ");
+		String bookName = scn.next();
+		
+			System.out.println("책 저자를 입력하세요");
+			String bookWriter = scn.next();
+			
+			System.out.println("출판사를 입력하세요");
+			String bookPrint = scn.next();
+			
+			System.out.println("가격을 입력해주세요");
+			int bookPrice = scn.nextInt();
+		
+			Book2 book = new Book2(bookName, bookWriter, bookPrint,  bookPrice);
+			 book.showInfo();
+	}
+
+}
+	
