dd68ac282fe5b36f194dec43d3eff1d5dc545548
user <user@DESKTOP-2203084>
2022-04-02 21:44:48 +0900

    0402 재시치고싶은데 안돼서 눈물나는중2

---
+++ P20220401/src/com/test/CollegeStudent.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class CollegeStudent {
+	private String profName; // 담당교수.
+	private String major; // 전공정보.
+
+	
+	
+}
---
+++ P20220401/src/com/test/HighStudent.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class HighStudent {
+	private String teacherName; // 담임선생님.
+	private String groupName; // 반이름.
+
+	
+}
