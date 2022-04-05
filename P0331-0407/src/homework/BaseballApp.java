2d6c3fd15f880a3f17048f72c6d9cf832367983a
admin <admin@YD-08>
2022-04-05 14:01:19 +0900

    0405 바보같은 돈세이브 ㅠㅠ

--- P0331-0407/src/homework/BaseballApp.java
+++ P0331-0407/src/homework/BaseballApp.java
@@ -25,23 +25,25 @@ public class BaseballApp extends DAO {
 //		 메뉴2 : (로그인 후) 1. 입력 2.전체목록조회 3. 게시글 번호로 조회 4. 게시글 번호 조회로 수정 //
 //		 5. 게시글 번호로 삭제 9. 로그아웃
 		while (true) {
-//			System.out.println("메뉴 : 1. 로그인 2.회원가입 3.탈퇴 9. 종료 ");
-//			System.out.println("선택 >> ");
+			System.out.println("메뉴 : 1. 로그인 2.회원가입 3.회원탈퇴 9. 종료 ");
+			System.out.println("선택 >> ");
 
-//			int menu = scn.nextInt();
-//			if (menu == 1) {// 로그인 기능
-//				System.out.println("ID 입력>>");
-//				String ID = scn.next();
-//				System.out.println("PW 입력>>");
-//				int PW = scn.nextInt();
-//				int result = 0;
-//
-//				if (result == 1) {
-//					System.out.println("로그인 되었습니다");
-//				} else if (result == 0) {
-//					System.out.println("로그인 실패.");
-//				}
-//				service.login(Login);
+			int menu = scn.nextInt();
+			if (menu == 1) {// 로그인 기능
+				
+				int result = 0;
+				System.out.println("ID 입력>>");
+				String ID = scn.next();
+				System.out.println("PW 입력>>");
+				int PW = scn.nextInt();
+				
+
+				if (result == 1) {
+					System.out.println("로그인 되었습니다");
+				} else if (result == 0) {
+					System.out.println("로그인 실패.");
+				}
+				service.login(Login);
 			// 로그인 이후 메뉴2
 			System.out.println("메뉴 : 1. 게시글입력 2. 전체목록조희 3. 게시글 번호로 조회" + "4. 수정 5. 삭제 9. 로그아웃");
 			System.out.println("선택 >> ");
@@ -114,30 +116,30 @@ public class BaseballApp extends DAO {
 
 			}
 
-//			} else if (menu == 2) {// 회원가입
-//
-//				System.out.println("가입할 아이디를 입력해주세요");
-//				String ID = scn.next();
-//				System.out.println("가입할 비밀번호를 입력해주세요");
-//				String PW = scn.next();
-//
-//				((Login) list2).setIdentify(ID);
-//				((Login) list2).setPass(PW);
-//
-//				service.addBaseBallMem(ID, PW);
-//
-//			} else if (menu == 3) {// 회원탈퇴
-//				System.out.println("탈퇴할 아이디를 입력해주세요");
-//				String ID = scn.next();
-//				System.out.println("탈퇴할 비밀번호를 입력해주세요");
-//				String PW = scn.next();
-//				service.deleteBaseballName(ID);
-//				service.deleteBaseballName(PW);
-//
-//			} else if (menu == 9) {
-//				// 로그인 없이 종료
-//
-//			}
+			} else if (menu == 2) {// 회원가입
+
+				System.out.println("가입할 아이디를 입력해주세요");
+				String ID = scn.next();
+				System.out.println("가입할 비밀번호를 입력해주세요");
+				String PW = scn.next();
+
+				((Login) list2).setIdentify(ID);
+				((Login) list2).setPass(PW);
+
+				service.addBaseBallMem(ID, PW);
+
+			} else if (menu == 3) {// 회원탈퇴
+				System.out.println("탈퇴할 아이디를 입력해주세요");
+				String ID = scn.next();
+				System.out.println("탈퇴할 비밀번호를 입력해주세요");
+				String PW = scn.next();
+				service.deleteBaseballName(ID);
+				service.deleteBaseballName(PW);
+
+			} else if (menu == 9) {
+				// 로그인 없이 종료
+
+			}
 
 		} // end of while
 
